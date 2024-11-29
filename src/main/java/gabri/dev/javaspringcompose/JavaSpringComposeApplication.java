07fba7cfccd0f0baa901f30506b71733ab5c75fd
Scipioni-Gabriel-405262 <405262@tecnicatura.frc.utn.edu.ar>
2024-11-29 12:01:24 -0300

    Initial commit

---
+++ src/main/java/gabri/dev/javaspringcompose/JavaSpringComposeApplication.java
@@ -0,0 +1,13 @@
+package gabri.dev.javaspringcompose;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringComposeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaSpringComposeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/gabri/dev/javaspringcompose/JavaSpringComposeApplicationTests.java
@@ -0,0 +1,13 @@
+package gabri.dev.javaspringcompose;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringComposeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
